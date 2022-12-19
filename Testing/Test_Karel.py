71650b64cab56744a2256bcb617108ae6bee6329
Karel Van Hoey <karel.vanhoey@ugent.be>
2022-12-19 18:51:58 +0100

    Fix naming of pictures

--- Testing/Test_Karel.py
+++ Testing/Test_Karel.py
@@ -1,5 +1,9 @@
 import numpy as np
 
-zero = np.zeros((5,10))
-print(zero)
-print(zero[4][9])
+a = np.linspace(0,0.001,21)
+print("a =", a)
+b = np.round(a*1000, 5)
+print("b =", b)
+for i in range(len(b)):
+    # print('{0:.2f}'.format(i))
+    print("Figures/PT@Time_"+"{0:.2f}".format(round(a[i]*1000,5))+"ms.png")
